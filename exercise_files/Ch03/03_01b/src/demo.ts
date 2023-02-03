c55826633229e604ea08d363aba76869e13dc4e4
martin.toman <martin.toman@twentysixdigital.com>
2023-02-03 16:33:25 +0100

    keyof testing

--- exercise_files/Ch03/03_01b/src/demo.ts
+++ exercise_files/Ch03/03_01b/src/demo.ts
@@ -1,39 +1,49 @@
 type ContactName = string;
 
-enum ContactStatus {
-    Active = "active",
-    Inactive = "inactive",
-    New = "new"
-}
+// enum ContactStatus {
+// 	Active = 'active',
+// 	Inactive = 'inactive',
+// 	New = 'new',
+// }
+
+// alternativa k Enum values - mnohem kratší a hezčí
+type ContactStatus = 'active' | 'inactive' | 'new';
+
+type ContactBirthDate = Date | number | string;
 
 interface Contact {
-    id: number;
-    name: ContactName;
-    birthDate?: Date;
-    status?: ContactStatus;
+	id: number;
+	name: ContactName;
+	birthDate?: ContactBirthDate;
+	status?: ContactStatus;
 }
 
 interface Address {
-    line1: string;
-    line2: string;
-    province: string;
-    region: string;
-    postalCode: string;
+	line1: string;
+	line2: string;
+	province: string;
+	region: string;
+	postalCode: string;
 }
 
+// one syntax
+interface AddressableContact extends Contact, Address {}
+
+// another syntax
+type AddressableContact2 = Contact & Address;
+
 function getBirthDate(contact: Contact) {
-    if (typeof contact.birthDate === "number") {
-        return new Date(contact.birthDate);
-    }
-    else if (typeof contact.birthDate === "string") {
-        return Date.parse(contact.birthDate)
-    }
-    else {
-        return contact.birthDate
-    }
+	if (typeof contact.birthDate === 'number') {
+		return new Date(contact.birthDate);
+	} else if (typeof contact.birthDate === 'string') {
+		return Date.parse(contact.birthDate);
+	} else {
+		return contact.birthDate;
+	}
 }
 
 let primaryContact: Contact = {
-    id: 12345,
-    name: "Jamie Johnson"
-}
+	id: 12345,
+	name: 'Jamie Johnson',
+	status: 'active',
+};
--- exercise_files/Ch03/03_02b/src/demo.ts
+++ exercise_files/Ch03/03_02b/src/demo.ts
@@ -1,16 +1,30 @@
 type ContactName = string;
-type ContactStatus = "active" | "inactive" | "new"
-type ContactBirthDate = Date | number | string
+type ContactStatus = 'active' | 'inactive' | 'new';
+type ContactBirthDate = Date | number | string;
 
-interface Contact  {
-    id: number;
-    name: ContactName;
-    birthDate?: ContactBirthDate;
-    status?: ContactStatus;
+interface Contact {
+	id: number;
+	name: ContactName;
+	birthDate?: ContactBirthDate;
+	status?: ContactStatus;
+	email?: string;
 }
 
 let primaryContact: Contact = {
-    id: 12345,
-    name: "Jamie Johnson",
-    status: "active"
+	id: 12345,
+	name: 'Jamie Johnson',
+	status: 'active',
+};
+
+type ContactFields = keyof Contact;
+
+const field: ContactFields = 'email';
+
+// Helpfull usage of keyof -> nemůžu posrat spelling v případě níže
+function getValue<T, U extends keyof T>(source: T, propertyName: U) {
+	return source[propertyName];
 }
+
+const value = getValue(primaryContact, 'status');
+
+const value2 = getValue({ min: 1, max: 200 }, ''); // samo to doplňuje z pole, díky použití <T>
